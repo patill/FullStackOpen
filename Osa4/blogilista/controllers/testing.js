e418f4b2ff13223e9d7460c6a4b30a6c91ae2e15
Till Paala <till.paala@pori.fi>
2024-08-27 14:23:18 +0300

    Osa4: correct testing api

--- Osa4/blogilista/controllers/testing.js
+++ Osa4/blogilista/controllers/testing.js
@@ -1,9 +1,9 @@
 const router = require("express").Router();
-const Note = require("../models/note");
-const User = require("../models/user");
+const Note = require("../models/Blog");
+const User = require("../models/User");
 
 router.post("/reset", async (request, response) => {
-  await Note.deleteMany({});
+  await Blog.deleteMany({});
   await User.deleteMany({});
 
   response.status(204).end();
