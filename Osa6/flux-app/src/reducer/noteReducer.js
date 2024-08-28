94e880f006b4d9efff2ed96de5ab779dd3d844bc
Till Paala <till.paala@pori.fi>
2024-08-28 15:03:40 +0300

    6: implement testing with jest for the first note app frontend

--- Osa6/flux-app/src/reducer/noteReducer.js
+++ Osa6/flux-app/src/reducer/noteReducer.js
@@ -1,31 +1,20 @@
-import { createStore } from "redux";
-
 const noteReducer = (state = [], action) => {
-  if (action.type === "NEW_NOTE") {
-    return state.concat(action.payload);
-  }
+  switch (action.type) {
+    case "NEW_NOTE":
+      return state.concat(action.payload);
 
-  return state;
+    case "TOGGLE_IMPORTANCE": {
+      const id = action.payload.id;
+      const noteToChange = state.find((n) => n.id === id);
+      const changedNote = {
+        ...noteToChange,
+        important: !noteToChange.important,
+      };
+      return state.map((note) => (note.id !== id ? note : changedNote));
+    }
+    default:
+      return state;
+  }
 };
 
-const store = createStore(noteReducer);
-
-store.dispatch({
-  type: "NEW_NOTE",
-  payload: {
-    content: "the app state is in redux store",
-    important: true,
-    id: 1,
-  },
-});
-
-store.dispatch({
-  type: "NEW_NOTE",
-  payload: {
-    content: "state changes are made with actions",
-    important: false,
-    id: 2,
-  },
-});
-
-export default store;
+export default noteReducer;
