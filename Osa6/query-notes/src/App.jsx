f2a2937177ca57b84d0fe505fffb279ebc13a2cb
Till Paala <till.paala@pori.fi>
2024-09-02 13:18:53 +0300

    6d: React query fetching the initial content without state

--- Osa6/query-notes/src/App.jsx
+++ Osa6/query-notes/src/App.jsx
@@ -1,32 +1,46 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
 const App = () => {
   const addNote = async (event) => {
-    event.preventDefault()
-    const content = event.target.note.value
-    event.target.note.value = ''
-    console.log(content)
-  }
+    event.preventDefault();
+    const content = event.target.note.value;
+    event.target.note.value = "";
+    console.log(content);
+  };
 
   const toggleImportance = (note) => {
-    console.log('toggle importance of', note.id)
+    console.log("toggle importance of", note.id);
+  };
+
+  const result = useQuery({
+    queryKey: ["notes"],
+    queryFn: () =>
+      axios.get("http://localhost:3001/notes").then((res) => res.data),
+  });
+
+  console.log(JSON.parse(JSON.stringify(result)));
+
+  if (result.isLoading) {
+    return <div>loading data...</div>;
   }
+  const notes = result.data;
 
-  const notes = []
-
-  return(
+  return (
     <div>
       <h2>Notes app</h2>
       <form onSubmit={addNote}>
         <input name="note" />
         <button type="submit">add</button>
       </form>
-      {notes.map(note =>
+      {notes.map((note) => (
         <li key={note.id} onClick={() => toggleImportance(note)}>
-          {note.content} 
-          <strong> {note.important ? 'important' : ''}</strong>
+          {note.content}
+          <strong> {note.important ? "important" : ""}</strong>
         </li>
-      )}
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
