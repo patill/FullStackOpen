316935a07b8ff16a32ee97399d3f2f59e887a9dd
Till Paala <till.paala@pori.fi>
2024-09-03 08:54:56 +0300

    Avoid get-request after post request when creating new notes

--- Osa6/query-notes/src/App.jsx
+++ Osa6/query-notes/src/App.jsx
@@ -5,8 +5,9 @@ const App = () => {
   const queryClient = useQueryClient();
   const newNoteMutation = useMutation({
     mutationFn: createNote,
-    onSuccess: () => {
-      queryClient.invalidateQueries('{ queryKey: ["notes"] }');
+    onSuccess: (newNote) => {
+      const notes = queryClient.getQueryData(["notes"]);
+      queryClient.setQueryData(["notes"], notes.concat(newNote));
     },
   });
   const addNote = async (event) => {
