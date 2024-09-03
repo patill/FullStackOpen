d08d6bdf2e6cf4d6cb377ef3cb4a7dc2996b9c7c
Till Paala <till.paala@pori.fi>
2024-09-03 11:31:44 +0300

    6d useReducer: providing a contextProvider to the app so the state of the context can be used everywhere

--- Osa6/useReducer-app/useReducer-app/src/App.jsx
+++ Osa6/useReducer-app/useReducer-app/src/App.jsx
@@ -1,20 +1,8 @@
-import { useReducer, useContext } from "react";
+import { useContext } from "react";
 import CounterContext from "./CounterContext";
+import { useCounterDispatch } from "./CounterContext";
 
 import "./App.css";
-
-const counterReducer = (state, action) => {
-  switch (action.type) {
-    case "INC":
-      return state + 1;
-    case "DEC":
-      return state - 1;
-    case "ZERO":
-      return 0;
-    default:
-      return state;
-  }
-};
 
 //Here the components:
 const Display = () => {
@@ -23,23 +11,19 @@ const Display = () => {
 };
 
 const Button = ({ type, label }) => {
-  const [counter, dispatch] = useContext(CounterContext);
+  const dispatch = useCounterDispatch();
   return <button onClick={() => dispatch({ type })}>{label}</button>;
 };
 
 function App() {
-  const [counter, counterDispatch] = useReducer(counterReducer, 0);
-
   return (
     <>
-      <CounterContext.Provider value={[counter, counterDispatch]}>
-        <Display />
-        <div>
-          <Button type={"INC"} label="+" />
-          <Button type={"DEC"} label="-" />
-          <Button type={"ZERO"} label="0" />
-        </div>
-      </CounterContext.Provider>
+      <Display />
+      <div>
+        <Button type={"INC"} label="+" />
+        <Button type={"DEC"} label="-" />
+        <Button type={"ZERO"} label="0" />
+      </div>
     </>
   );
 }
--- Osa6/useReducer-app/useReducer-app/src/CounterContext.jsx
+++ Osa6/useReducer-app/useReducer-app/src/CounterContext.jsx
@@ -1,5 +1,40 @@
-import { createContext } from "react";
+import { createContext, useContext, useReducer } from "react";
+
+const counterReducer = (state, action) => {
+  switch (action.type) {
+    case "INC":
+      return state + 1;
+    case "DEC":
+      return state - 1;
+    case "ZERO":
+      return 0;
+    default:
+      return state;
+  }
+};
 
 const CounterContext = createContext();
+
+export const CounterContextProvider = (props) => {
+  const [counter, counterDispatch] = useReducer(counterReducer, 0);
+
+  return (
+    <CounterContext.Provider value={[counter, counterDispatch]}>
+      {props.children}
+    </CounterContext.Provider>
+  );
+};
+
+//helper functions to import only one of the two arguments in the list:
+//custom hooks
+export const useCounterValue = () => {
+  const fullList = useContext(CounterContext);
+  return fullList[0];
+};
+
+export const useCounterDispatch = () => {
+  const fullList = useContext(CounterContext);
+  return fullList[1];
+};
 
 export default CounterContext;
