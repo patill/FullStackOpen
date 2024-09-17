12c33d8fbb0eaffd0d665c0181dc141207e88331
Till Paala <till.paala@pori.fi>
2024-09-17 13:23:00 +0300

    7e: react app with class component logic

---
+++ Osa7/Course-material/Class_components/src/App.jsx
@@ -0,0 +1,38 @@
+import "./App.css";
+
+import React from "react";
+import axios from "axios";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { anecdotes: [], current: 0 };
+
+    this.componentDidMount = () => {
+      axios.get("http://localhost:3001/anecdotes").then((response) => {
+        this.setState({ anecdotes: response.data });
+      });
+    };
+
+    this.handleClick = () => {
+      const current = Math.floor(Math.random() * this.state.anecdotes.length);
+      this.setState({ current });
+    };
+  }
+
+  render() {
+    if (this.state.anecdotes.length === 0) {
+      return <div>no anecdotes...</div>;
+    }
+    return (
+      <div>
+        <h1>anecdote of the day</h1>
+        <div> {this.state.anecdotes[this.state.current].content} </div>{" "}
+        <button onClick={this.handleClick}>next</button>
+      </div>
+    );
+  }
+}
+
+export default App;
