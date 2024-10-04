7550e57e7a3867f65ef39808e49ca862fd2ed0a2
Till Paala <till.paala@pori.fi>
2024-10-04 15:06:04 +0300

    8a: more mutations and sophisticated queries

--- Osa8/graphql-project/index.js
+++ Osa8/graphql-project/index.js
@@ -40,9 +40,14 @@ const typeDefs = `
     id: ID!
   }
 
+  enum YesNo {
+    YES
+    NO
+  }
+
   type Query {
     personCount: Int!
-    allPersons: [Person!]!
+    allPersons(phone: YesNo): [Person!]!
     findPerson(name: String!): Person
   }
 
@@ -53,13 +58,25 @@ const typeDefs = `
       street: String!
       city: String!
       ): Person
+      editNumber(
+      name: String!
+      phone: String!): Person
   }
+
+  
 `;
 
 const resolvers = {
   Query: {
     personCount: () => persons.length,
-    allPersons: () => persons,
+    allPersons: (root, args) => {
+      if (!args.phone) {
+        return persons;
+      }
+      const byPhone = (person) =>
+        args.phone === "YES" ? person.phone : !person.phone;
+      return persons.filter(byPhone);
+    },
     findPerson: (root, args) => persons.find((p) => p.name === args.name),
   },
   Person: {
@@ -83,6 +100,14 @@ const resolvers = {
       const person = { ...args, id: uuid() };
       persons = persons.concat(person);
       return person;
+    },
+    editNumber: (root, args) => {
+      const person = persons.find((p) => p.name === args.name);
+      if (!person) return null;
+
+      const updatedPerson = { ...person, phone: args.phone };
+      persons = persons.map((p) => (p.name === args.name ? updatedPerson : p));
+      return updatedPerson;
     },
   },
 };
