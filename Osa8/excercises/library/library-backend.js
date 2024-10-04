1ed5c9d97c43f155f371cd5c3988f0896c43f81e
Till Paala <till.paala@pori.fi>
2024-10-04 15:58:55 +0300

    8.5 all books by author or genre

--- Osa8/excercises/library/library-backend.js
+++ Osa8/excercises/library/library-backend.js
@@ -116,7 +116,7 @@ const typeDefs = `
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks(author: String): [Book!]!
+    allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
 `;
@@ -132,9 +132,15 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: (root, args) => {
-      if (!args.author) return books;
-
-      return books.filter((book) => book.author === args.author);
+      if (args.author && args.genre)
+        return books
+          .filter((book) => book.author === args.author)
+          .filter((book) => book.genres.includes(args.genre));
+      if (args.author)
+        return books.filter((book) => book.author === args.author);
+      if (args.genre)
+        return books.filter((book) => book.genres.includes(args.genre));
+      return books;
     },
     allAuthors: () => authors,
   },
